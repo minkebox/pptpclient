9bb780d93fc1529142aa3a0c84dc53ea3db5c001
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2020-03-07 15:23:43 -0800

    Hand configure

--- root/startup.sh
+++ root/startup.sh
@@ -1,13 +1,18 @@
 #! /bin/sh
 
-HOME_INTERFACE=${__HOME_INTERFACE}
-INTERNAL_INTERFACE=${__PRIVATE_INTERFACE}
-EXTERNAL_INTERFACE=tun0
-
-/usr/sbin/pptpsetup --create ${HOSTNAME} --server ${SERVER} --username ${USER} --password ${PASSWORD} --encrypt
+/bin/mknod /dev/ppp c 108 0
+cat > /etc/ppp/peers/${HOSTNAME} << __EOF__
+lock
+pty "pptp ${SERVER} --nolaunchpppd"
+name ${USER}
+password ${PASSWORD}
+remotename PPTP
+ipparam "${HOSTNAME}"
+require-mppe-128
+__EOF__
 /usr/sbin/pppd call ${HOSTNAME}
 
-trap "killall sleep pptpsetup pppd dnsmasq miniupnpd; exit" TERM INT
+trap "killall sleep pptpsetup perl pppd dnsmasq miniupnpd; exit" TERM INT
 
 sleep 2147483647d &
 wait "$!"
