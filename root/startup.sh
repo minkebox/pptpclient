c71f34e5f704b416752add3f11cf55686cd9ac00
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2020-03-07 15:40:53 -0800

    Add missing quotes

--- root/startup.sh
+++ root/startup.sh
@@ -4,15 +4,15 @@
 cat > /etc/ppp/peers/${HOSTNAME} << __EOF__
 lock
 pty "pptp ${SERVER} --nolaunchpppd"
-name ${USER}
-password ${PASSWORD}
+name "${USER}"
+password "${PASSWORD}"
 remotename PPTP
 ipparam "${HOSTNAME}"
 require-mppe-128
 __EOF__
 /usr/sbin/pppd call ${HOSTNAME}
 
-trap "killall sleep pptpsetup perl pppd dnsmasq miniupnpd; exit" TERM INT
+trap "killall sleep pptpsetup perl pppd pptp dnsmasq miniupnpd; exit" TERM INT
 
 sleep 2147483647d &
 wait "$!"
