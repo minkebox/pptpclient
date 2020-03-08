95859177a1b8dd866c7718047d3188d642132519
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2020-03-07 16:32:22 -0800

    Refuse simple authenticators

--- root/startup.sh
+++ root/startup.sh
@@ -1,16 +1,22 @@
 #! /bin/sh
 
 /bin/mknod /dev/ppp c 108 0
-cat > /etc/ppp/peers/${HOSTNAME} << __EOF__
+
+cat > /etc/ppp/peers/vpn << __EOF__
 lock
 pty "pptp ${SERVER} --nolaunchpppd"
 name "${USER}"
 password "${PASSWORD}"
 remotename PPTP
-ipparam "${HOSTNAME}"
+ipparam "vpn"
 require-mppe-128
+refuse-pap
+refuse-eap
+refuse-chap
 __EOF__
-/usr/sbin/pppd call ${HOSTNAME}
+
+#/usr/sbin/pppd call vpn debug dump logfd 2 nodetach persist
+/usr/sbin/pppd call vpn
 
 trap "killall sleep pptpsetup perl pppd pptp dnsmasq miniupnpd; exit" TERM INT
 
