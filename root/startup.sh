58659e80e3d16e6c36a6652bb9cde8ee5d51b490
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2020-03-07 20:12:38 -0800

    Improve configuration

--- root/startup.sh
+++ root/startup.sh
@@ -2,16 +2,43 @@
 
 /bin/mknod /dev/ppp c 108 0
 
+if [ "${PAP}" = "true" ]; then
+  PAP=""
+else
+  PAP="refuse-pap"
+fi
+if [ "${EAP}" = "true" ]; then
+  EAP=""
+else
+  EAP="refuse-eap"
+fi
+if [ "${CHAP}" = "true" ]; then
+  CHAP=""
+else
+  CHAP="refuse-chap"
+fi
+if [ "${MSCHAP}" = "true" ]; then
+  MSCHAP=""
+else
+  MSCHAP="refuse-mschap"
+fi
+if [ "${MPPE}" = "true" ]; then
+  MPPE="require-mppe-128"
+else
+  MPPE=""
+fi
+
 cat > /etc/ppp/peers/vpn << __EOF__
 pty "pptp ${SERVER} --nolaunchpppd"
 name "${USER}"
 password "${PASSWORD}"
 remotename PPTP
 ipparam "vpn"
-#require-mppe-128
-refuse-pap
-refuse-eap
-refuse-chap
+${MPPE}
+${PAP}
+${EAP}
+${CHAP}
+${MSCHAP}
 __EOF__
 
 #/usr/sbin/pppd call vpn debug dump logfd 2 nodetach persist
